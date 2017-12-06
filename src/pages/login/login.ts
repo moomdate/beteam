b3580bc573615526b40424661433e920d7ecc8ae
Surasak sincharoen <moomdate@gmail.com>
2017-12-06 16:16:44 +0700

    login design

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -21,6 +21,9 @@ export class LoginPage {
 
   ) { }
 
+  test(){
+    console.log("test");
+  }
   loginFacebook() {
     if (this.platform.is('cordova')) {
       return this.facebook.login(['email', 'public_profile']).then(res => {
@@ -34,27 +37,7 @@ export class LoginPage {
         .then(res => console.log(res));
     }
   }
-  signInWithFacebook() {
-    if (this.platform.is('cordova')) {
-      return this.facebook.login(['email', 'public_profile']).then(res => {
-        const facebookCredential = firebase.auth.FacebookAuthProvider.credential(res.authResponse.accessToken);
-        return firebase.auth().signInWithCredential(facebookCredential);
-      })
 
-    }
-    else {
-      /* return this.afAuth.auth
-         .signInWithPopup(new firebase.auth.FacebookAuthProvider())
-         .then(res => {
-           if (res.operationType == 'signIn')
-             //this.navCtrl.setRoot(HomePage);
-         }).catch(res => {
-           //this.user.email = res['email'];
-         });
-     }*/
-      console.log("please check again");
-    }
-  }
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
   }
